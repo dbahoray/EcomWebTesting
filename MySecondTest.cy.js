f63251fda0ccabc8acd822b6b4ff800f4a4dbd7d
dbahoray <66402251+dbahoray@users.noreply.github.com>
2024-11-24 10:57:29 +0530

    Update MySecondTest.cy.js

--- MySecondTest.cy.js
+++ MySecondTest.cy.js
@@ -1,15 +1,25 @@
 /// <reference types="Cypress" />
+import 'cypress-file-upload'
 
 describe('MySecondTest', () => {
-    it('Enter valid credentials', () =>{
 
+    beforeEach('function', () =>{
         cy.on('uncaught:exception', (err, runnable) => {
 
             return false
         })
 
         cy.visit("https://mccoymart.com/")
-        cy.get("#profile-menu-btn").click()
+    })
+    it('Enter valid credentials', () =>{
+
+        // cy.on('uncaught:exception', (err, runnable) => {
+
+        //     return false
+        // })
+
+        // cy.visit("https://mccoymart.com/")
+        cy.get(".loginSignUpBtn").click()
         cy.get("input[placeholder='Mobile Number']").type("9205489292")
         cy.get("button[class='blue-btn-global loginSignupBtnStep1']").click()
         cy.get("form[id='loginSignupFormStep2'] input[placeholder='Enter OTP']").type("8118")
@@ -27,15 +37,15 @@ describe('MySecondTest', () => {
     })
     it('Enter invalid credentials', () => {
 
-        cy.on('uncaught:exception', (err, runnable) => {
+        // cy.on('uncaught:exception', (err, runnable) => {
 
-            return false
-        })
+        //     return false
+        // })
 
-        const mobileNumber = "990009"
+        const mobileNumber = "9900001111112"
 
-        cy.visit("https://mccoymart.com/")
-        cy.get("#profile-menu-btn").click()
+       // cy.visit("https://mccoymart.com/")
+        cy.get(".loginSignUpBtn").click()
 
         cy.get("input[placeholder='Mobile Number']").type(mobileNumber)
         cy.get("button[class='blue-btn-global loginSignupBtnStep1']").click()
@@ -46,16 +56,17 @@ describe('MySecondTest', () => {
         else if(mobileNumber.length < 10){
             cy.get("span[class='errorMsg']").should('have.text', 'Phone must consist of at least 10 digits')
         }
+        cy.wait(3000)
         // else {
         //     cy.get("span[class='errorMsg']").should('not.exist')
         // }
     })
     it('Validate shop products', () => {
-        cy.visit("https://mccoymart.com/")
-        cy.get(".home-offering-card.home-shop-card.d-block").click()
+       // cy.visit("https://mccoymart.com/")
+        cy.get("a[class='home-offering-card home-shop-card d-block']").click()
         cy.url().should('eq','https://mccoymart.com/buy/')
         
-        cy.xpath("//body[1]/section[2]/div[1]/div[2]/div[1]/div[1]/ul[1]/li[1]/div[1]/a")
+        cy.xpath("//body[1]/section[4]/div[1]/div[2]/div[1]/div[1]/ul[1]/li[1]/div[1]/div[1]/a[1]")
         .then((b) => {
 
             let url = b.prop('href')
@@ -66,9 +77,28 @@ describe('MySecondTest', () => {
 
         cy.go('back')
     })
+    it('Validate credit finance', () => {
+       // cy.visit("https://mccoymart.com/")
+        cy.get("a[class='home-offering-card home-shop-card d-block  test-irshad......']").should('be.visible').click()
+        cy.url().should('eq','https://mccoymart.com/credit-finance/')
+        cy.get("div[class='bannerBtnWrapper'] button[type='button']").click()
+        cy.get(".customSelectBtn").trigger('mouseover').click()
+        cy.get("li[value='10-50 Lacs']").should('be.visible').click()
+        cy.get("#name").type("Testing101")
+        const mnumber = "9205489292"
+        cy.get("#mobile").type(mnumber)
+        cy.get("#save").click()
+        if(mnumber.length < 10){
+            cy.get("#mobile_message").should('have.text','Please enter your mobile.')
+        }
+        cy.get("#mobile_otp").type("8118")
+        cy.get("#save_otp").click()
+        cy.xpath("//h5[normalize-space()='Thank you']").should('have.text','Thank you')
+        cy.get("a[class='formBtnOrange']").click()
+    })
     it('Validate footer links', () => {
 
-        cy.visit("https://mccoymart.com/")
+       // cy.visit("https://mccoymart.com/")
         const sections = ["//h3[normalize-space()='Shop Products']",
                           "//h3[normalize-space()='Find Professional']",
                           "//h3[normalize-space()='Buy Leads']",
@@ -86,7 +116,7 @@ describe('MySecondTest', () => {
     })
     it('Validate Shop Products links', () => {
 
-        cy.visit("https://mccoymart.com/")
+        //cy.visit("https://mccoymart.com/")
 
         cy.xpath("(//ul[@class='about_company'])[1]//li").should('have.length', '9')
         cy.xpath("(//ul[@class='about_company'])[1]//li").each(($el, index, $list) => {
@@ -97,7 +127,7 @@ describe('MySecondTest', () => {
     })
     it('Validate Find professional links', () => {
 
-        cy.visit("https://mccoymart.com/")
+        //cy.visit("https://mccoymart.com/")
 
         cy.xpath("//div[@class='details_about_company_foot ']//div[2]//li").should('have.length', '8')
         cy.xpath("//div[@class='details_about_company_foot ']//div[2]//li")
@@ -124,7 +154,7 @@ describe('MySecondTest', () => {
     })
     it('Validate Buy Leads links', () => {
 
-        cy.visit("https://mccoymart.com/")
+        //cy.visit("https://mccoymart.com/")
 
         cy.xpath("(//div[@class='about_company-wrapper'])[3]//li").should('have.length', '10')
         cy.xpath("(//ul[@class='about_company'])[3]//li").each(($el,index,$list) => {
@@ -150,7 +180,7 @@ describe('MySecondTest', () => {
     })
     it('Validate Company links', () => {
 
-        cy.visit("https://mccoymart.com/")
+        //cy.visit("https://mccoymart.com/")
 
         cy.xpath("//div[@class='details_about_company_foot ']//div[4]//li").should('have.length', '10')
         .each(($el,index,$list) => {
@@ -176,9 +206,9 @@ describe('MySecondTest', () => {
             }
         })
     })
-    it('Click on Bulk Orders', () =>{
-        cy.visit("https://mccoymart.com/")
-        cy.xpath("//a[normalize-space()='Bulk Orders']").click()
+    it.only('Click on Bulk Orders', () =>{
+        //cy.visit("https://mccoymart.com/")
+        cy.get("a[class='WhatsApp btn my-btn']").click()
 
         let EName = "Get a Quote instantly"
         cy.xpath("//h2[normalize-space()='Get a Quote instantly']").then( (z) => {
@@ -196,7 +226,7 @@ describe('MySecondTest', () => {
         cy.get("input[placeholder='Qty']").type("2")
         cy.get("button[class='next rfq-next-btn step1Btn']").click()
         cy.xpath("//input[@id='quote_location']").type("508213")
-        cy.wait(3000)
+        cy.wait(2000)
         cy.xpath("//ul[@class='location-quotes-dropdown-inner']//li").click()
         cy.get("button[class='next rfq-btn-str rfq-next-btn step7Btn']").click()
 
@@ -225,17 +255,18 @@ describe('MySecondTest', () => {
                                   //BDD Style
                                   expect(aname2).to.equal(ename2)
         })
+        cy.get(":nth-child(13) > .quote-dtls-head > div > :nth-child(1)").scrollIntoView({duration:3000})
     })
-    it.only('Validate Brands', () => {
+    it('Validate Brands', () => {
 
-        cy.visit("https://mccoymart.com/")
+        //cy.visit("https://mccoymart.com/")
         cy.get(".border-0.viewallbtnslide").click()
 
         cy.url().should('include', 'https://mccoymart.com/buy/brands/')
 
         cy.xpath("//div[@id='search_brand_list']//div").each(($el,index,$list) => {
 
-            if(index < 50){
+            if(index < 25){
                 cy.log('Clicking link:',$el.text())
                 cy.xpath("//div[@id='search_brand_list']//div").eq(index).should('be.visible')
                 .find('a')
